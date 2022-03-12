b54a2200fb152723acdd46776ab1968d2cb1aec7
Jonathan Martin <jomartin@udel.edu>
2022-03-12 15:36:06 -0500

    Tests up and running

--- Assets/_SCRIPTS_/tests/UtilitiesTests.cs
+++ Assets/_SCRIPTS_/tests/UtilitiesTests.cs
@@ -15,10 +15,33 @@ public class UtilitiesTests
         // Use the Assert class to test conditions
     }
 
-    // A Test behaves as an ordinary method
     [Test]
-    public void TestMajor(){
-        // Use the Assert class to test conditions
-        
+    public void TestMajorNormal(){
+        // Normal condition
+        fp3 yMajor = new fp3(3,11,.5m);
+        fp expected = 11;
+
+        fp actual = yMajor.major();
+        Assert.That(expected == actual, "Incorrect major calculation");
+    }
+
+    [Test]
+    public void TestMajorTie(){
+        // Tie
+        fp3 tieMajor = new fp3(0,5,5);
+        fp expected = 5;
+
+        fp actual = tieMajor.major();
+        Assert.AreEqual(expected, actual, "Incorrect major calculation");
+    }
+
+    [Test]
+    public void TestMajorZero(){
+        // Tie
+        fp3 zeroMajor = new fp3(0,0,0);
+        fp expected = 0;
+
+        fp actual = zeroMajor.major();
+        Assert.AreEqual(expected, actual, "Incorrect major calculation");
     }
 }
